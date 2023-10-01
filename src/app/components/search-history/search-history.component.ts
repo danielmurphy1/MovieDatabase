1fb21eedebf97ff8f9d080aa3171b475fca3a3db
danielmurphy1 <daniel.murphy.1@gmail.com>
2023-10-01 19:00:48 -0400

    added content to search-history with hardcoded values

--- src/app/components/search-history/search-history.component.ts
+++ src/app/components/search-history/search-history.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-search-history',
   templateUrl: './search-history.component.html',
   styleUrls: ['./search-history.component.css']
 })
-export class SearchHistoryComponent {
+export class SearchHistoryComponent implements OnInit{
+  historyItems = [
+    {
+      searchString: "Test", 
+      date: new Date().toLocaleString(),
+      results: 17
+    },
+    {
+      searchString: "Test1", 
+      date: new Date().toLocaleString(),
+      results: 17
+    },
+    {
+      searchString: "Test2", 
+      date: new Date().toLocaleString(),
+      results: 17
+    },
+  ]
+  constructor() {}
 
+  ngOnInit(): void {
+    
+  }
 }
